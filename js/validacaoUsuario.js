00e781861a4dc29624dd13b2a3d919e79714ef4c
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2020-09-14 23:15:57 -0300

    Criado carregamento dos dados ao se logar com metodo onload() do JavaScript

--- js/validacaoUsuario.js
+++ js/validacaoUsuario.js
@@ -1,55 +1,55 @@
-let emailValidacao = document.querySelector("#emailValidacao");
-let cpfValidacao = document.querySelector("#cpfValidacao");
-let forma = document.querySelector("#validacaoUsuario");
+// let emailValidacao = document.querySelector("#emailValidacao");
+// let cpfValidacao = document.querySelector("#cpfValidacao");
+// let forma = document.querySelector("#validacaoUsuario");
 
-// fetch('https://aw-api-biblioteca.herokuapp.com/api/13/livros').then(function(response){
-//     response.json().then(function(data) {
-//         console.log(data);
-//     });
-// }).catch(function(error) {
-//     console.log('Fetch Error:', error);
-// });
+// // fetch('https://aw-api-biblioteca.herokuapp.com/api/13/livros').then(function(response){
+// //     response.json().then(function(data) {
+// //         console.log(data);
+// //     });
+// // }).catch(function(error) {
+// //     console.log('Fetch Error:', error);
+// // });
 
-forma.addEventListener("submit", function(event){
-	event.preventDefault();
+// forma.addEventListener("submit", function(event){
+// 	event.preventDefault();
 
-	let dadosa = {
-		email: emailValidacao.value,
-		cpf: cpfValidacao.value
-	};
+// 	let dadosa = {
+// 		email: emailValidacao.value,
+// 		cpf: cpfValidacao.value
+// 	};
 
-	if(emailValidacao.value == null || emailValidacao.value == ""){
-		alert("Email inválido, por favor informe novamente.")
-	}else if(cpfValidacao.value == null || cpfValidacao.value == "" ){
-		alert("CPF inválido, por favor informe novamente.")
-	}else{
-		fetch('https://aw-api-biblioteca.herokuapp.com/api', {
-			method: 'POST',
-			supportHeaderParams: true,
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(dadosa)
-		})
-		.then(function(response) {
-			return response
-		})
-		.then(function(response) {
-			if(!response.ok){
-				throw Error(response.statusText);
-			}else{
-				alert("Login efetuado com sucesso.")
-				// const json = '{"result":true, "count":42}';
-				const json1 = '{"message":{"success":true,"details":"Login efetuado com sucesso.","id":4}}';
-				const obj = JSON.stringify(json1);
-				idUsuario = obj.message.id;
-				urlUsuario = 'https://aw-api-biblioteca.herokuapp.com/api/cliente/' + idUsuario;
-				alert(obj.message.id)
-				// window.location.href = "usuario.html";
-			}	
-		})
-		.catch(e => alert("Email ou CPF inválido."))
-	}
-})
+// 	if(emailValidacao.value == null || emailValidacao.value == ""){
+// 		alert("Email inválido, por favor informe novamente.")
+// 	}else if(cpfValidacao.value == null || cpfValidacao.value == "" ){
+// 		alert("CPF inválido, por favor informe novamente.")
+// 	}else{
+// 		fetch('https://aw-api-biblioteca.herokuapp.com/api', {
+// 			method: 'POST',
+// 			supportHeaderParams: true,
+// 			headers: {
+// 				'Accept': 'application/json',
+// 				'Content-Type': 'application/json'
+// 			},
+// 			body: JSON.stringify(dadosa)
+// 		})
+// 		.then(function(response) {
+// 			return response
+// 		})
+// 		.then(function(response) {
+// 			if(!response.ok){
+// 				throw Error(response.statusText);
+// 			}else{
+// 				alert("Login efetuado com sucesso.")
+// 				// const json = '{"result":true, "count":42}';
+// 				const json1 = '{"message":{"success":true,"details":"Login efetuado com sucesso.","id":4}}';
+// 				const obj = JSON.stringify(json1);
+// 				idUsuario = obj.message.id;
+// 				urlUsuario = 'https://aw-api-biblioteca.herokuapp.com/api/cliente/' + idUsuario;
+// 				alert(obj.message.id)
+// 				// window.location.href = "usuario.html";
+// 			}	
+// 		})
+// 		.catch(e => alert("Email ou CPF inválido."))
+// 	}
+// })
 
