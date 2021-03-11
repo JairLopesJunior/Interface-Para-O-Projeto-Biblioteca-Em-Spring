c2e140b96a3e3730b862bd1d85c6faabcb7cf29b
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-03-10 23:57:21 -0300

    Adicionado Sweet Alert na tela de Login

--- js/validacaoUsuario.js
+++ js/validacaoUsuario.js
@@ -6,36 +6,84 @@ var idUsuario;
 formV.addEventListener("submit", function(event){
 	event.preventDefault();
 
-	let dadosV = {
-		email: emailV.value,
-		cpf: cpfV.value
-	};
+	let str = cpfV.value; 
+	let exp = /[0-9]/g;
+	let result = str.match(exp);
+	let r = JSON.stringify(result)
+	r = r.replace(/[^\d]+/g, '')
 
-	fetch('http://localhost:8080/api', {
-		method: 'POST',
-		supportHeaderParams: true,
-		headers: {
-    		'Accept': 'application/json',
-        	'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(dadosV)
-	})
-	.then(function(response) {
-		if(!response.ok){
-			throw Error(response.statusText);
-		}else{
-			//const json1 = '{"message":{"success":true,"details":"Login efetuado com sucesso.","id":13}}';
-			//const obj = JSON.parse(json1);
-			//idUsuario = obj.message.id;
-			return response.json()
-		}
-	})
-	.then((response) => {
-		//const obj = JSON.parse(response);
-		idUsuario = response.message.id;
-		alert("Login efetuado com sucesso.")
-		window.location.href = `${idUsuario}/usuario.html`;
-	})
-	.catch(e => alert("Email ou CPF inválido."))
+	if(str.length !== r.length){
+		Swal.fire({
+		  	type: "error",
+		  	icon: "error",
+		  	text: "O campo CPF aceita somente números!!",
+		  	showConfirmButton: false,
+		  	timer: 2000
+		})
+	}else if(emailV.value === null || emailV.value === ""){
+		Swal.fire({
+		  	type: "error",
+		  	icon: "error",
+		  	text: "O campo Email precisa ser preenchido!!",
+		  	showConfirmButton: false,
+		  	timer: 2000
+		})
+	}else if(cpfV.value === null || cpfV.value === ""){
+		Swal.fire({
+		  	type: "error",
+		  	icon: "error",
+		  	text: "O campo CPF precisa ser preenchido!!",
+		  	showConfirmButton: false,
+		  	timer: 2000
+		})
+	}else{
+
+		let dadosV = {
+			email: emailV.value,
+			cpf: cpfV.value
+		};
+
+		fetch('http://localhost:8080/api', {
+			method: 'POST',
+			supportHeaderParams: true,
+			headers: {
+	    		'Accept': 'application/json',
+	        	'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(dadosV)
+		})
+		.then(function(response) {
+			if(!response.ok){
+				throw Error(response.statusText);
+			}else{
+				//const json1 = '{"message":{"success":true,"details":"Login efetuado com sucesso.","id":13}}';
+				//const obj = JSON.parse(json1);
+				//idUsuario = obj.message.id;
+				return response.json()
+			}
+		})
+		.then((response) => {
+			//const obj = JSON.parse(response);
+			idUsuario = response.message.id;
+			Swal.fire({
+			  type: "success",
+			  icon: "success",
+			  text: "Login efetuado com sucesso!!",
+			  showConfirmButton: false,
+			  timer: 2000
+			});
+
+			setTimeout(function(){ 
+				window.location.href = `${idUsuario}/usuario.html`;
+			}, 2000);
+		})
+		.catch(e => Swal.fire({
+					  	type: "error",
+					  	icon: "error",
+					  	text: "Dados inválidos!!",
+					  	showConfirmButton: false,
+					  	timer: 2000
+					})
+			  )
+	}
 })
-
