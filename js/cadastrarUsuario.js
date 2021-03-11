c3c38516d8cbef44f37a480b678aa74f6e191ebb
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-03-10 21:38:36 -0300

    Adicionado Cadastro de Livros

--- js/cadastrarUsuario.js
+++ js/cadastrarUsuario.js
@@ -1,6 +1,6 @@
 let nome = document.querySelector("#nomeInformado");
 let cpf = document.querySelector("#cpfInformado");
-let celular = document.querySelector("#celularInformado");
+let telefone = document.querySelector("#telefoneInformado");
 let email = document.querySelector("#emailInformado");
 let bairro = document.querySelector("#bairroInformado");
 let rua = document.querySelector("#ruaInformado");
@@ -13,49 +13,33 @@ form.addEventListener("submit", function(event){
     let dados = {
 		nome: nome.value,
         cpf: cpf.value,
-        celular: celular.value,
+        telefone: telefone.value,
         email: email.value,
         bairro: bairro.value,
         rua: rua.value,
         numero: numero.value
-	};
-	
-	if(nome.value == null || nome.value == ""){
-		alert("Por favor, informe o campo Nome.")
-	}else if(cpf.value == null || cpf.value == ""){
-		alert("Por favor, informe o campo CPF.")
-	}else if(email.value == null || email.value == ""){
-		alert("Por favor, informe o campo Email.")
-	}else if(celular.value == null || celular.value == ""){
-		alert("Por favor, informe o campo Telefone.")
-	}else if(bairro.value == null || bairro.value == ""){
-		alert("Por favor, informe o campo Bairro.")
-	}else if(rua.value == null || rua.value == ""){
-		alert("Por favor, informe o campo Rua.")
-	}else if(numero.value == null || numero.value == ""){
-		alert("Por favor, informe o campo Numero.")
-	}else{
-		fetch('https://aw-api-biblioteca.herokuapp.com/api/clientes', {
-			method: 'POST',
-			supportHeaderParams: true,
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(dados)
-		})
-		.then(function(response) {
-			return response
-		})
-		.then(function(response) {
-			if(!response.ok){
-				throw Error(response.statusText);
-			}else{
-				alert("Usuário cadastrado com sucesso.")
-				myFunction();
-			}
-		})
-		.catch(e => alert("Dados incorretos, por favor informe corretamente."))
-	}
+    };
+
+	fetch('http://localhost:8080/api/clientes', {
+		method: 'POST',
+		supportHeaderParams: true,
+		headers: {
+    		'Accept': 'application/json',
+        	'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(dados)
+	})
+	.then(function(response) {
+		return response
+	})
+	.then(function(response) {
+		if(!response.ok){
+			throw Error(response.statusText);
+		}else{
+			alert("Usuário cadastrado com sucesso.")
+			window.location.href = "home.html";
+		}
+	})
+	.catch(e => alert("Dados incorretos, por favor informe corretamente."))
 })
 
--- js/validacaoUsuario.js
+++ js/validacaoUsuario.js
@@ -1,54 +1,41 @@
-let emailValidacao = document.querySelector("#emailValidacao");
-let cpfValidacao = document.querySelector("#cpfValidacao");
-let forma = document.querySelector("#validacaoUsuario");
+let emailV = document.querySelector("#emailValidacao");
+let cpfV = document.querySelector("#cpfValidacao");
+let formV = document.querySelector("#validacaoUsuario");
 var idUsuario;
 
-// fetch('https://aw-api-biblioteca.herokuapp.com/api/13/livros').then(function(response){
-//     response.json().then(function(data) {
-//         console.log(data);
-//     });
-// }).catch(function(error) {
-//     console.log('Fetch Error:', error);
-// });
-
-forma.addEventListener("submit", function(event){
+formV.addEventListener("submit", function(event){
 	event.preventDefault();
 
-	let dadosa = {
-		email: emailValidacao.value,
-		cpf: cpfValidacao.value
+	let dadosV = {
+		email: emailV.value,
+		cpf: cpfV.value
 	};
 
-	if(emailValidacao.value == null || emailValidacao.value == ""){
-		alert("Email inválido, por favor informe novamente.")
-	}else if(cpfValidacao.value == null || cpfValidacao.value == "" ){
-		alert("CPF inválido, por favor informe novamente.")
-	}else{
-		fetch('https://aw-api-biblioteca.herokuapp.com/api', {
-			method: 'POST',
-			supportHeaderParams: true,
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(dadosa)
-		})
-		.then(function(response) {
-			return response
-		})
-		.then(function(response) {
-			if(!response.ok){
-				throw Error(response.statusText);
-			}else{
-				alert("Login efetuado com sucesso.")
-				// const json = '{"result":true, "count":42}';
-				const json1 = '{"message":{"success":true,"details":"Login efetuado com sucesso.","id":4}}';
-				const obj = JSON.parse(json1);
-				idUsuario = obj.message.id;
-				window.location.href = "usuario.html"+idUsuario;
-			}	
-		})
-		.catch(e => alert("Email ou CPF inválido."))
-	}
+	fetch('http://localhost:8080/api', {
+		method: 'POST',
+		supportHeaderParams: true,
+		headers: {
+    		'Accept': 'application/json',
+        	'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(dadosV)
+	})
+	.then(function(response) {
+		if(!response.ok){
+			throw Error(response.statusText);
+		}else{
+			//const json1 = '{"message":{"success":true,"details":"Login efetuado com sucesso.","id":13}}';
+			//const obj = JSON.parse(json1);
+			//idUsuario = obj.message.id;
+			return response.json()
+		}
+	})
+	.then((response) => {
+		//const obj = JSON.parse(response);
+		idUsuario = response.message.id;
+		alert("Login efetuado com sucesso.")
+		window.location.href = `${idUsuario}/usuario.html`;
+	})
+	.catch(e => alert("Email ou CPF inválido."))
 })
 
